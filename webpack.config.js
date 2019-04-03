fd5d312c3e8cd67a6762e7831cef9aec316f0317
Quang Tran <quang.tran@numberfour.eu>
2019-04-03 11:26:55 +0200

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+//  const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+  	// Webpack starts calculating dependency graph from this file
+    app: './src-gen/client/main.js'
+  },
+  output: {
+  	// The output concatenated file
+    filename: 'vote-app-redux.js',
+    path: path.resolve(__dirname, 'public/dist')
+  },
+  devtool: 'inline-source-map', 
+  /** Configure Webpack Dev server */
+  devServer: {
+  	// Serve files from the `public` folder at localhost:8080 
+	contentBase: path.join(__dirname, 'public'),
+	port: 8080,
+	hot: true,
+	writeToDisk: true
+  },
+
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],  
+
+  resolve: {
+    alias: {
+	 	"vote-example-redux": __dirname
+	}
+  }
+};
